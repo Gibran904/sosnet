d5f109e73a1f89606d69601cc9db6f02f75af99a
NotGibran <64770138+Gibran904@users.noreply.github.com>
2025-06-28 17:57:20 +0700

    Create provider.tsx

---
+++ app/provider.tsx
@@ -0,0 +1,3 @@
+const [supabase] = useState(() =>
+  createBrowserClient('"https://neurxtlohabhnwfuduql.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldXJ4dGxvaGFiaG53ZnVkdXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMDUxODYsImV4cCI6MjA2NjY4MTE4Nn0.mc7s5chhuHriv8b6Pna1hfI0PkVWp3LKyTSWvICr_fw"', )
+)
