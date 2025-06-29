e726dd6340d6048e0018472a07a84f4811a32a63
NotGibran <64770138+Gibran904@users.noreply.github.com>
2025-06-29 08:28:04 +0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,5 +1,13 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { type SupabaseClient } from '@supabase/supabase-js'
+import { Database } from '@/types/supabase'
 
-export const createClient = () =>
-  createServerComponentClient({ cookies })
+export const createClient = (): SupabaseClient<Database> =>
+  createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies,
+    }
+  )
